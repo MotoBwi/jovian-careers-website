ad0cf38e6c726f77012ea0526a6f402dd26e08c0
Bapan Sardar <sbapan41@gmail.com>
2024-12-11 17:40:52 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,40 +2,11 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remot'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$120,000'
-  }
-]
 
 @app.route("/")
 def hello_jovian():
-    return render_template('home.html',
-                           jobs=JOBS, 
-                          company_name='Jovian')
-@app.route("/api/jobs")
-def list_jobs():
-  return jsonify(JOBS)
+    return render_template('home.html', 
+                          company_name="Bapan's")
   
 
 if __name__ == "__main__":
